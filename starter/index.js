67317216cfa7abb2a530bfb5ea2301c99d9cc9bf
oguzhanulun <ulunoguzhan@gmail.com>
2023-03-06 17:23:29 +0300

    routing exercise codes added

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ///////////////////////////////////////////
 /// FİLES *************
@@ -32,7 +33,21 @@ const http = require("http");
 ////SERVER *********************
 
 const server = http.createServer((req, res) => {
-  res.end("hello from the server");
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("this is OVERVİEW");
+  } else if (pathName === "/product") {
+    res.end("this is PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found<h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
