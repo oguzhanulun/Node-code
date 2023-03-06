5f01b3b15cf1a6bf41f5bb28467704e8cc2cc6bb
oguzhanulun <ulunoguzhan@gmail.com>
2023-03-06 15:22:12 +0300

    file system file reading and writing code added

--- starter/index.js
+++ starter/index.js
@@ -1,9 +1,25 @@
-const fs = require("fs")
+const fs = require("fs");
+// Blocking Synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8") //readFileSync iki eleman alır.Birincisi dosya yolu(path), ikinci is karakter encoding(utf-8 gibi) için kullanılır(buffer)
+// console.log(textIn)
+// const textOut = `This is what we know about the avocado :  ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("file written!");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8") //readFileSync iki eleman alır.Birincisi dosya yolu(path), ikinci is karakter encoding(utf-8 gibi) için kullanılır(buffer)
-console.log(textIn)
+// Non-blocking, asynchronous way
 
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!");
 
-const textOut = `This is what we know about the avocado :  ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("file written!");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile(".txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("your file has been written");
+      });
+    });
+  });
+});
+console.log("will read file");
