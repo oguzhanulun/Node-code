e0503c2341492af5795474eaf1a705aa866d4abd
oguzhanulun <ulunoguzhan@gmail.com>
2023-03-07 16:20:45 +0300

    async-await codes added

--- asynchronous-JS/index.js
+++ asynchronous-JS/index.js
@@ -4,7 +4,7 @@ const superagent = require("superagent");
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
-      if (err) reject("ı could not find that file");
+      if (err) reject("I could not find that file");
       resolve(data);
     });
   });
@@ -13,12 +13,58 @@ const readFilePro = (file) => {
 const writeFilePro = (file, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, (err) => {
-      if (err) reject("ı could not write file");
+      if (err) reject("I could not write file");
       resolve("success");
     });
   });
 };
 
+//ASYNC -- AWAİT CODES----------------
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed : ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro("dog-image.txt", res.body.message);
+    console.log("random dog image saved");
+  } catch (err) {
+    console.log(err.message);
+
+    throw err.message;
+  }
+  return "2: READY !!!";
+};
+
+(async () => {
+  try {
+    console.log("1:will get dog pics");
+    const x = await getDogPic();
+    console.log(x);
+    console.log("3:done getting dog pics");
+  } catch (err) {
+    console.log("!!!!!!ERROR!!!!!!!");
+  }
+})();
+
+/*
+console.log("1:will get dog pics");
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log("3:done getting dog pics");
+  })
+  .catch((err) => {
+    console.log("!!!!!!ERROR!!!!!!!");
+  });
+*/
+
+//////////////PROMİSE CODES///////////
+/*
 readFilePro(`${__dirname}/dog.txt`)
   .then((result) => {
     console.log(`Breed : ${result}`);
@@ -34,3 +80,4 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch((err) => {
     console.log(err.message);
   });
+*/
