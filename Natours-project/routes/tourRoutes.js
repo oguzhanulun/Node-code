398369c4888dd028823567277d92db513c720ebe
oguzhanulun <ulunoguzhan@gmail.com>
2023-03-14 14:35:21 +0300

    mongo AGGREGATION codes added

--- Natours-project/routes/tourRoutes.js
+++ Natours-project/routes/tourRoutes.js
@@ -6,11 +6,15 @@ const {
   deleteTour,
   createTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const tourRouter = express.Router();
 
 tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+tourRouter.route('/tour-stats').get(getTourStats);
+tourRouter.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 // tourRouter.param('id', checkID);
 
