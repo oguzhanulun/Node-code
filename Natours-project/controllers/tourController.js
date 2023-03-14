87a53e7819cf3907ad124edd938762f938db8674
oguzhanulun <ulunoguzhan@gmail.com>
2023-03-14 11:30:36 +0300

    pagination codes added

--- Natours-project/controllers/tourController.js
+++ Natours-project/controllers/tourController.js
@@ -37,20 +37,36 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
-    //{difficulty:"easy", duration:{$gte:5}}>> gte = greater then and equal
-    // gte, gt, lte, lt
-    /*
-    // const tours = await Tour.find({
-    //   duration: 5,
-    //   difficulty: easy,
-    // });
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals(5);
-    */
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //3) Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 4) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 5) Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist.');
+    }
+
     //EXECUTE QUERY
     const tours = await query;
 
