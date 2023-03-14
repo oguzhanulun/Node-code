ba6c7a82ce6ba09aa5c60dfd468961bb87c88dd6
oguzhanulun <ulunoguzhan@gmail.com>
2023-03-14 15:58:49 +0300

    custom validation codes added

--- Natours-project/models/tourModel.js
+++ Natours-project/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+//const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must be less or equal than 40 characters'],
       minlength: [10, 'A tour name must be more or equal than 10 characters'],
+      //validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     duration: {
       type: Number,
@@ -41,7 +43,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a name'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          //this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       required: [true, 'A tour must have a description'],
