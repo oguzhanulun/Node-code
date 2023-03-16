971a92994df438d70a144759a60e05cd4ee92499
oguzhanulun <ulunoguzhan@gmail.com>
2023-03-16 14:17:26 +0300

    global error handler codes added

--- Natours-project/app.js
+++ Natours-project/app.js
@@ -1,8 +1,11 @@
-const fs = require('fs');
+// const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 //MIDDLEWARES
@@ -11,18 +14,18 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('hello from the middleware');
+//   next();
+// });
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+// );
 
 //app.get('/api/v1/tours', getAllTours);
 //app.get('/api/v1/tours/:id', getTour);
@@ -33,6 +36,14 @@ const tours = JSON.parse(
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-// START SERVER
+
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Cant find ${req.originalUrl} on this server !!!`);
+  // err.status = 'Fail';
+  // err.statusCode = 404;
+  next(new AppError(`Cant find ${req.originalUrl} on this server !!!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
